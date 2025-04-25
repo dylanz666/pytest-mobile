9be4997617a482c6adc03dbfaf4cd9580f2d83b6
dyzhang <997604787@qq.com>
2025-04-25 16:18:01 +0800

    feat: add jsonpath_util.py

--- tools/utils.py
+++ tools/utils.py
@@ -2,6 +2,7 @@ from tools.config_util import ConfigUtil
 from tools.crypto_util import CryptoUtil
 from tools.data_util import DataUtil
 from tools.file_util import FileUtil
+from tools.jsonpath_util import JSONPathUtil
 from tools.random_util import RandomUtil
 from tools.time_util import TimeUtil
 
@@ -13,6 +14,7 @@ class Utils:
     file_util = FileUtil
     random_util = RandomUtil
     time_util = TimeUtil
+    jsonpath_util = JSONPathUtil
 
 
 if __name__ == "__main__":
